5703ce68d334231082171f3a467040de156435af
Anggarda Gasta <anggarda.gasta.p@gmail.com>
2017-08-21 11:49:43 +0700

    Deleting unnecessary directory

--- rest/dbConnection.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-		host:'localhost',
-		user:'root',
-		password:'password',
-		database:'nodejs'
-	});
-module.exports = connection;
